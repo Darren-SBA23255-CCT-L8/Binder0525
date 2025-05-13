c62c148d68d354763da756a8366f544b6f174549
Darren-SBA23255-CCT-L8 <sba23255@student.cct.ie>
2025-05-13 19:30:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting the Jupyter Notebook..."
-jupyter nbconvert --to notebook --execute Dashboard_Binder.ipynb --output Dashboard_Binder.ipynb
+jupyter nbconvert --to notebook --execute dashboard.ipynb --output dashboard_executed.ipynb
